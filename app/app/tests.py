85ea7e802868d1da9da8415030052043d8c111a0
jas <jasbir.singh@tradebyte.com>
2023-04-19 13:25:50 +0200

    Changed voulme path

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, 5)
